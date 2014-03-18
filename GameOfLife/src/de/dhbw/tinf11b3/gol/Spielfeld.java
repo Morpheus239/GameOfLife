7a1d9de738942a46b0c4dffa3017f655dfaac887
michael <michael@ubuntu>
2014-03-18 19:52:22 +0100

    Nachbar

---
+++ GameOfLife/src/de/dhbw/tinf11b3/gol/Spielfeld.java
@@ -0,0 +1,15 @@
+package de.dhbw.tinf11b3.gol;
+
+public class Spielfeld {
+
+	public void setAlive(int i, int j) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isAlive(int i, int j) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ GameOfLife/test/de/dhbw/tinf11b3/gol/NachbarTest.java
@@ -0,0 +1,38 @@
+package de.dhbw.tinf11b3.gol;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NachbarTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void belebeSpieler() throws Exception{
+		
+		Spielfeld spielfeld = new Spielfeld();
+		spielfeld.setAlive(0,0);
+		
+		assertTrue("player is not alive", spielfeld.isAlive(0,0));
+	}
+
+}
