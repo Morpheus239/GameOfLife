5923390bade09405a768b216ef2cc33bd387c632
michael <michael@ubuntu>
2014-03-18 18:53:52 +0100

    Fix it #1

---
+++ GameOfLife/test/de/dhbw/tinf11b3/gol/RulesTest.java
@@ -0,0 +1,35 @@
+package de.dhbw.tinf11b3.gol;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RulesTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void birth() {
+		boolean nextState = new Rules().nextStateOf(false, 3);
+		assertTrue("nothing was born", nextState);
+	}
+
+}
