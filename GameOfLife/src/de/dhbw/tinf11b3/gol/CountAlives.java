9779c05592d2b55b626b6fa890e5948a8c5d38ec
Frank Mueller <frmimue@gmail.com>
2014-03-18 20:43:25 +0100

    :

--- GameOfLife/src/de/dhbw/tinf11b3/gol/CountAlives.java
+++ GameOfLife/src/de/dhbw/tinf11b3/gol/CountAlives.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class CountAlives {
 
 	public static Integer forGiven(List<Boolean> states) {
-		return 999;
+		int count = 0;
+		for(Boolean state : states)
+			count += state ? 1 : 0;
+		return count;
 	}
 
 }
